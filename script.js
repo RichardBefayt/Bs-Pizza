bd52a050b9a0a111962f4dffd56416b1791d6296
Richard Befayt <you@example.comsprichie.rb@gmail.com>
2022-09-25 19:16:18 +0200

    Compte et Commandes initialisés

--- script.js
+++ script.js
@@ -4,6 +4,24 @@ document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
 }
 
+let account = document.querySelector('.user-account');
+
+document.querySelector('#user-btn').onclick = () => {
+    account.classList.add('active');
+}
+document.querySelector('#close-account').onclick = () => {
+    account.classList.remove('active');
+}
+
+let orders = document.querySelector('.my-orders');
+
+document.querySelector('#order-btn').onclick = () => {
+    orders.classList.add('active');
+}
+document.querySelector('#close-orders').onclick = () => {
+    orders.classList.remove('active');
+}
+
 window.onscroll = () =>{
     navbar.classList.remove('active');
  };
